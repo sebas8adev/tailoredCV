eca305a14ef28024f201d265b5828f10e0b27635
Sebas 8a <sebas8adev@gmail.com>
2026-03-02 17:37:39 -0500

    03.02

--- 0_LinkedIn_Networking/scrape_linkedin_networking_bot.py
+++ 0_LinkedIn_Networking/scrape_linkedin_networking_bot.py
@@ -169,7 +169,7 @@ def wish_birthdays(driver):
     ]
 
     wished_count = 0
-    daily_limit = random.randint(3, 8)
+    daily_limit = random.randint(2, 6)
     print(f"Today's birthday wish limit is set to {daily_limit}.")
     max_tabs_without_finding_new_person = random.randint(50, 150)
     tabs_count = 0
@@ -290,7 +290,7 @@ def like_job_changes(driver):
     actions = ActionChains(driver)
 
     liked_count = 0
-    daily_limit = random.randint(3, 6)
+    daily_limit = random.randint(2, 6)
     print(f"Today's job change like limit is set to {daily_limit}.")
     max_tabs_without_finding_new_card = random.randint(80, 350)
     tabs_count = 0
@@ -351,7 +351,7 @@ def like_work_anniversaries(driver):
     actions = ActionChains(driver)
 
     liked_count = 0
-    daily_limit = random.randint(3, 6)
+    daily_limit = random.randint(2, 6)
     print(f"Today's work anniversary like limit is set to {daily_limit}.")
     max_tabs_without_finding_new_card = random.randint(80, 350)
     tabs_count = 0
@@ -412,7 +412,7 @@ def like_education_updates(driver):
     actions = ActionChains(driver)
 
     liked_count = 0
-    daily_limit = random.randint(3, 7)
+    daily_limit = random.randint(2, 6)
     print(f"Today's education update like limit is set to {daily_limit}.")
     max_tabs_without_finding_new_card = random.randint(80, 350)
     tabs_count = 0
